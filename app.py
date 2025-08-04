f50f8d536bb015a0954ce9e3dcc675355133b715
skazei <siarheikazei33@gmail.com>
2025-08-04 09:28:57 +0200

    add ico and redirect engine endpoint

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, send_from_directory
 
 app = Flask(__name__)
+
+SEARCH_ENGINES = {
+    'google': 'https://www.google.com/search?q={query}',
+    'bing': 'https://www.bing.com/search?q={query}',
+    'yahoo': 'https://search.yahoo.com/search?p={query}',
+    'ask': 'https://www.ask.com/web?q={query}',
+    'duckduckgo': 'https://duckduckgo.com/?q={query}',
+    'startpage': 'https://www.startpage.com/do/search?query={query}'
+}
 
 @app.route('/search/', methods=['GET'])
 def search_endpoint():
@@ -9,3 +18,21 @@ def search_endpoint():
         return redirect(f"https://www.google.com/search?q={query}", code=302)
     else:
         return redirect("https://www.google.com/", code=302)
+
+@app.route('/search/<engine>/', methods=['GET'])
+def dynamic_search_endpoint(engine):
+    query = request.args.get('q')
+
+    if not query:
+        return redirect(SEARCH_ENGINES['google'].format(query=''), code=302)
+
+    if engine in SEARCH_ENGINES:
+        redirect_url = SEARCH_ENGINES[engine].format(query=query)
+        return redirect(redirect_url, code=302)
+    else:
+        redirect_url = SEARCH_ENGINES['google'].format(query=query)
+        return redirect(redirect_url, code=302)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.ico', mimetype='image/x-icon')
